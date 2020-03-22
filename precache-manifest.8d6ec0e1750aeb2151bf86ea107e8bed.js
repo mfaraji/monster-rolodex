49a5ab5e19b4fdd6e51d3056a1726ad5f71fe360
Moe Faraji <faraji66@gmail.com>
2020-03-21 20:10:17 -0400

    Updates

---
+++ precache-manifest.8d6ec0e1750aeb2151bf86ea107e8bed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b6a2e2be70f0224964ea4b31954994a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25985ec8f88c39714c91",
+    "url": "/static/css/main.3b8e0bfe.chunk.css"
+  },
+  {
+    "revision": "01fa59518f48c0c9b0f6",
+    "url": "/static/js/2.650d6c94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.650d6c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25985ec8f88c39714c91",
+    "url": "/static/js/main.40f7e00a.chunk.js"
+  },
+  {
+    "revision": "863018210bf1c6ae9c68",
+    "url": "/static/js/runtime-main.215bd54d.js"
+  }
+]);
